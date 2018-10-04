d0b1e44da40770c332665233233933cd166eb129
bryanttsang <43279760+bryanttsang@users.noreply.github.com>
2018-10-04 14:51:15 -0400

    Update TestFile.java

--- src/BTsangLib/TestFile.java
+++ src/BTsangLib/TestFile.java
@@ -2,24 +2,8 @@ package BTsangLib;
 
 public class TestFile {
 
-    public static String vigCipher(String message, String key)
+    public static void main(String[] args)
     {
-        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        int length = message.length();
-        String encoded = "";
-        int shift = Integer.parseInt(key);
-        int letter = 0;
-        for (int i = 0; i < length; i++)
-        {
-            if ((alphabet.indexOf(message.charAt(i)) + shift) > 25)
-            {
-                letter = alphabet.indexOf(message.charAt(i)) - 26 + shift;
-            }
-            else
-            {
-                letter = alphabet.indexOf(message.charAt(i)) + shift;
-            }
-            encoded = encoded + alphabet.charAt(letter);
-        }
+        System.out.println(BTsangLib.isPalindrome("racecar"));
     }
 }
